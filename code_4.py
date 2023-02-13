6ee3e118cb6c904e0cdfc9ca9c3c9e42c0185ecd
jdelarosa10 <60360825+jdelarosa10@users.noreply.github.com>
2023-02-12 23:35:01 -0700

    WIP1
    Initial work on code. Applying what I currently know to try and solve the problem. Current Issues are:
    - don't know how to get code for countries
    - don't know how to get column index to retrieve information that functions need.

--- code_4.py
+++ code_4.py
@@ -1,18 +1,36 @@
 import csv
-import pandas as pd
-big_mac_file = './big-mac-full-index.csv'
+#import pandas as pd
+big_mac_file = open('./big-mac-full-index.csv')
 
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
-
+    pass
+    '''
+    for i in big_mac_file:
+        if i is year and i is country_code:
+            avgPrice = 
+    '''
 def get_big_mac_price_by_country(country_code):
     pass # Remove this line and code your function
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    min = 0
+    for i in big_mac_file:
+        if i == 'dollar price':
+            min = int(i)
+    return min
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    max = 0
+    getIndex = 0
+    for i in big_mac_file:
+        if i == 'dollar price':
+            getIndex = getIndex + 1
+    return getIndex
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    userYear = int(input("Enter the year you want to search for: "))
+    userCountry = (input("Enter the country you want to search for: "))
+    #need to convert userCountry to code 
+    get_big_mac_price_by_year(userYear, userCountry)
+    print(get_the_cheapest_big_mac_price_by_year(userYear))
+    print(get_the_most_expensive_big_mac_price_by_year(userYear))

5b5275a58446834533c44cca2f5ce492a5b043e9
jdelarosa10 <60360825+jdelarosa10@users.noreply.github.com>
2023-02-20 10:09:57 -0700

    push2
    Got max and min working, but they are retrieving the max and min of the entire data set instead of the year that is being passed to the method.

--- code_4.py
+++ code_4.py
@@ -1,36 +1,35 @@
 import csv
-#import pandas as pd
-big_mac_file = open('./big-mac-full-index.csv')
+import pandas as pd
+big_mac_file = './big-mac-full-index.csv'
 
 def get_big_mac_price_by_year(year,country_code):
     pass
-    '''
-    for i in big_mac_file:
-        if i is year and i is country_code:
-            avgPrice = 
-    '''
+
 def get_big_mac_price_by_country(country_code):
     pass # Remove this line and code your function
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    min = 0
-    for i in big_mac_file:
-        if i == 'dollar price':
-            min = int(i)
-    return min
+    '''
+    queryString = f"(iso_a3 == {year})"
+    leastExp = round(df['dollar_price'].min(),2)
+    idx = df["dollar_price"].idxmin()
+    leastExpCountry = df.loc[idx]
+    print(f"{leastExpCountry['name']}({leastExpCountry['iso_a3']}): {leastExp}")
+    '''
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    max = 0
-    getIndex = 0
-    for i in big_mac_file:
-        if i == 'dollar price':
-            getIndex = getIndex + 1
-    return getIndex
-
+    queryString = df['date'].at.year == year
+    df_Year = df.query(queryString)
+    print(df_Year)
+    '''
+    queryYear = df.query(queryString)
+    print(queryYear)
+    mostExp = round(df['dollar_price'].max(),2)
+    idx = df["dollar_price"].idxmax()
+    mostExpCountry = df.loc[idx]
+    print(f"{mostExpCountry['name']}({mostExpCountry['iso_a3']}): {mostExp}")
+    '''
 if __name__ == "__main__":
-    userYear = int(input("Enter the year you want to search for: "))
-    userCountry = (input("Enter the country you want to search for: "))
-    #need to convert userCountry to code 
-    get_big_mac_price_by_year(userYear, userCountry)
-    print(get_the_cheapest_big_mac_price_by_year(userYear))
-    print(get_the_most_expensive_big_mac_price_by_year(userYear))
+    df = pd.read_csv(big_mac_file)
+    userYear = 2005
+    get_the_most_expensive_big_mac_price_by_year(userYear)

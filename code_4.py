3c64e62c7a6ef4298aaf99d5cf98611ff2e9857b
jdelarosa10 <60360825+jdelarosa10@users.noreply.github.com>
2023-02-24 19:59:58 -0700

    WIP4

--- code_4.py
+++ code_4.py
@@ -9,24 +9,17 @@ def get_big_mac_price_by_country(country_code):
     pass # Remove this line and code your function
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    '''
-    queryString = f"(iso_a3 == {year})"
-    leastExp = round(df['dollar_price'].min(),2)
-    idx = df["dollar_price"].idxmin()
-    leastExpCountry = df.loc[idx]
-    print(f"{leastExpCountry['name']}({leastExpCountry['iso_a3']}): {leastExp}")
-    '''
+    pass 
 
 def get_the_most_expensive_big_mac_price_by_year(year):
     queryString = f"date >= '{year}-01-01' and date <= '{year}-12-31'"
     df_Year = df.query(queryString)
-    print(df_Year)
-    mostExp = round(df['dollar_price'].max(),2)
-    idx = df["dollar_price"].idxmax()
-    mostExpCountry = df.loc[idx]
-    print(f"{mostExpCountry['name']}({mostExpCountry['iso_a3']}): {mostExp}")
-
+    min_df_idx = df['dollar_price'].idxmin()
+    min_item = df.loc[min_df_idx]
+    print(f"{min_item['name']}({min_item['iso_a3']}):")
+    #: {round(min_item['dollarprice'],2)}")
+    
 if __name__ == "__main__":
     df = pd.read_csv(big_mac_file)
-    userYear = 2005
+    userYear = 2000
     get_the_most_expensive_big_mac_price_by_year(userYear)
